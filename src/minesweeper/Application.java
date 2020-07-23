32b78f21dc4ea7b93244d88834322836ad7c408f
thxwelchs <thxwelchs@gmail.com>
2020-07-24 01:56:35 +0900

    [UPDATE] rollback application class

--- src/minesweeper/Application.java
+++ src/minesweeper/Application.java
@@ -14,22 +14,22 @@ public class Application {
     private static final boolean MINE_FLAG_MODE = false;
 
     public static void main(String[] args) {
-//        Land land = new Land(ROW, COL, MINE_COUNT);
-//        land.render(MINE_FLAG_MODE);
+        Land land = new Land(ROW, COL, MINE_COUNT);
+        land.render(MINE_FLAG_MODE);
 
         // Mines Mocking test
-        Land land = new Land(ROW, COL, Arrays.stream(new int[][]{
-                {1, 2},
-                {4, 5},
-                {1, 9},
-                {2, 9},
-                {7, 8},
-                {5, 1},
-                {8, 8},
-                {3, 2},
-                {4, 0},
-                {7, 3}
-        }).map(mine -> new Mine(mine[0], mine[1])).toArray(Mine[]::new));
-        land.render(true);
+//        Land land = new Land(ROW, COL, Arrays.stream(new int[][]{
+//                {1, 2},
+//                {4, 5},
+//                {1, 9},
+//                {2, 9},
+//                {7, 8},
+//                {5, 1},
+//                {8, 8},
+//                {3, 2},
+//                {4, 0},
+//                {7, 3}
+//        }).map(mine -> new Mine(mine[0], mine[1])).toArray(Mine[]::new));
+//        land.render(true);
     }
 }
